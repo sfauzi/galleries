32775c79fe01f374e95bde93194fb3527c55c855
sfauzi <hello@sfauzi.dev>
2026-04-23 19:02:35 +0700

    feat: :sparkles: initial setup

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="bg-stone-50 dark:bg-stone-950 text-stone-900 dark:text-stone-300 transition-colors duration-300">
+        <main class="flex-1">
+            <slot />
+        </main>
+    </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1 class="text-center">Hello world</h1>
+</template>
